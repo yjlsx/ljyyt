c14b609ace6388ae34f3b118a8f61ae8f7b9035b
yjlsx <2413704955@qq.com>
2026-04-22 22:08:52 +0800

    .

--- site-config.js
+++ site-config.js
@@ -1,3 +1,3 @@
-window.LYRICS_API_ENDPOINT = window.LYRICS_API_ENDPOINT || '';
-window.LYRICS_SEARCH_API_ENDPOINT = window.LYRICS_SEARCH_API_ENDPOINT || '';
-window.COVER_API_ENDPOINT = window.COVER_API_ENDPOINT || '';
+window.LYRICS_API_ENDPOINT = 'https://ljyyt-api.yjlsx0.workers.dev/api/lyrics';
+window.LYRICS_SEARCH_API_ENDPOINT = 'https://ljyyt-api.yjlsx0.workers.dev/api/lyrics/search';
+window.COVER_API_ENDPOINT = 'https://ljyyt-api.yjlsx0.workers.dev/api/cover';
